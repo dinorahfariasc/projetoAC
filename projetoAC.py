a77acdbb45ef04d8e3623f8e62fedc44f8a1570b
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2023-06-29 10:29:03 -0300

    Update projetoAC

--- projetoAC.py
+++ projetoAC.py
@@ -30,3 +30,43 @@ from itertools import *
 #     print(list(y))
 #
 # print(f"o total do jogos sera: {len(jogos)}")
+
+# ======================================================================
+
+# print("\n================ 2.3 Questao 27 ===================")
+# from itertools import combinations
+# # Jogadores disponíveis
+# jogadores = ["Jogador1", "Jogador2", "Jogador3", "Jogador4", "Jogador5",
+#              "Jogador6", "Jogador7", "Jogador8", "Jogador9", "Jogador10",
+#              "Jogador11", "Jogador12", "Jogador13", "Jogador14", "Jogador15"]
+
+# # Chama a função para dividir os jogadores em 3 times
+# print("\nopcoes equipe esperança\n")
+# esperança = combinations(jogadores, 5)
+# count = 0
+# for x in esperança:
+#   print(list(x))
+#   count += 1
+# print(count)
+
+# # como 5 jogadores ja foram selecionados, para o proximo grupo sobraram 10
+# opcoesConfianca = [1,2,3,4,5,6,7,8,9,10]
+
+# confianca = combinations(opcoesConfianca,5)
+# count2 = 0
+# for x in confianca:
+#   print(list(x))
+#   count2 += 1
+# print(count2)
+
+# print("\nopcoes equipe vitoria")
+# opcoesVitoria = [1,2,3,4,5]
+
+# vitoria = combinations(opcoesVitoria,5)
+# count3 = 0
+# for x in vitoria:
+#   print(list(x))
+#   count3 += 1
+# print(count3)
+
+# print(f"\no total de times possiveis será de: {count * count2 * count3}")
